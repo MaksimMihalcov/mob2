fe62b5034f2aaa1e6c0cb31edf876bfb5cd37e1e
MaksimMihalcov <maksiswet@gmail.com>
2023-02-14 22:53:51 +0300

    init

---
+++ Mob2.1/Mob2.1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mob2._1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            //colView.ItemsSource = await App.NotesDB.GetAllNotesAsync();
+        }
+    }
+}
